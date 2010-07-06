f9314164325618e4cee62c9d20b84a7ab911d918
Joe.zhou <zhendong.zhou@gmail.com>
2010-07-06 20:28:45 +0800

    add computing square roots with newton's method

---
+++ computing_square_roots_with_newton_method.scm
@@ -0,0 +1,35 @@
+;; The first three lines of this file were inserted by DrScheme. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname computing_square_roots_with_newton_method) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+(define (square x) (* x x))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.0001))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x)
+                 x)))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+
+
+(define (sqrt_func x)
+  (sqrt-iter 1.0 x))
+
+(define a (sqrt_func 2))
+
+;test
+a
+
+(* a a)
+
+
+
+
